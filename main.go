b3d45d00674256df71fd085075a9c6167a1070e1
bilang <xiaoyaobilang@gmail.com>
2019-11-13 17:35:24 +0800

    从node.db中自动解析bcode 设置了一些参数的默认值

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/jessevdk/go-flags"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -12,22 +14,31 @@ type Options struct {
 	Watch   bool `long:"watch" description:"Watch for interface changes"`
 
 	// Netlink Interface
-	Interface       string `long:"interface" short:"i" value-name:"IFACE" description:"Use address from interface"`
-	InterfaceFamily Family `long:"interface-family" value-name:"ipv4|ipv6|all" description:"Limit to interface addreses of given family"`
+	Interface       string `long:"interface" default:"tun0" short:"i" value-name:"IFACE" description:"Use address from interface"`
+	InterfaceFamily Family `long:"interface-family" default:"ipv6" value-name:"ipv4|ipv6|all" description:"Limit to interface addreses of given family"`
 
 	// DNS Update
-	Server        string        `long:"server" value-name:"HOST[:PORT]" description:"Server for UPDATE query, default is discovered from zone SOA"`
+	Server        string        `long:"server" default:"ns.nodes.bonuscloud.work" value-name:"HOST[:PORT]" description:"Server for UPDATE query, default is discovered from zone SOA"`
 	Timeout       time.Duration `long:"timeout" value-name:"DURATION" default:"10s" description:"Timeout for sever queries"`
 	Retry         time.Duration `long:"retry" value-name:"DURATION" default:"30s" description:"Retry interval, increased for each retry attempt"`
-	TSIGName      string        `long:"tsig-name" value-name:"FQDN"`
-	TSIGSecret    string        `long:"tsig-secret" value-name:"BASE-64" env:"TSIG_SECRET" description:"base64-encoded shared TSIG secret key"`
-	TSIGAlgorithm TSIGAlgorithm `long:"tsig-algorithm" value-name:"hmac-{md5,sha1,sha256,sha512}" default:"hmac-sha1."`
-	Zone          string        `long:"zone" value-name:"FQDN" description:"Zone to update, default is derived from name"`
+	TSIGName      string        `long:"tsig-name" default:"nodes.bonuscloud.work" value-name:"FQDN"`
+	TSIGSecret    string        `long:"tsig-secret" default:"/mpNYBgjQUD1ZY9lFRGDabdZu0jxypHIJCI4HquSeEL1IVeuqB6rsc/wBLATpG8XngZHJBCSgkUWfRbjPL/MIA==" value-name:"BASE-64" env:"TSIG_SECRET" description:"base64-encoded shared TSIG secret key"`
+	TSIGAlgorithm TSIGAlgorithm `long:"tsig-algorithm" default:"hmac-md5" value-name:"hmac-{md5,sha1,sha256,sha512}" default:"hmac-sha1."`
+	Zone          string        `long:"zone" default:"nodes.bonuscloud.work." value-name:"FQDN" description:"Zone to update, default is derived from name"`
 	TTL           time.Duration `long:"ttl" value-name:"DURATION" default:"60s" description:"TTL for updated records"`
 
 	Args struct {
 		Name string `value-name:"FQDN" description:"DNS Name to update"`
 	} `positional-args:"yes"`
+}
+
+const (
+	SerializeFile = "/opt/bcloud/node.db"
+)
+
+type Node struct {
+	Bcode string `json:"bcode"`
+	Email string `json:"email"`
 }
 
 func main() {
@@ -37,6 +48,27 @@ func main() {
 		log.Fatalf("flags.Parse: %v", err)
 		os.Exit(1)
 	}
+
+	if options.Args.Name == "" {
+		j, err := ioutil.ReadFile(SerializeFile)
+		if err != nil {
+			log.Fatalf("fail to get name")
+			os.Exit(1)
+		}
+
+		if len(j) == 0 {
+			log.Fatalf("fail to get name")
+			os.Exit(1)
+		}
+		var node Node
+		err = json.Unmarshal(j, &node)
+		if err != nil {
+			log.Fatalf("fail to get name")
+			os.Exit(1)
+		}
+		options.Args.Name = node.Bcode + "." + options.Zone
+	}
+	log.Printf("get name %s", options.Args.Name)
 
 	var update = Update{
 		ttl:     int(options.TTL.Seconds()),
